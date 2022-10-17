496484dc757fbac3e0e62b33302ba594dfdc2f33
Saleh Alawsh <44799605+alawsh21@users.noreply.github.com>
2022-10-17 10:38:54 +0300

    Add files via upload
    %%%% Those codes maybe submitted with the orginal paper to the Jouranl %%%%

--- Reedme.m
+++ Reedme.m
@@ -2,3 +2,4 @@
 %%%%%%%%%%%%%%%% This code was writen by Dr. Saleh Alawsh %%%%%%%%%%%%%%%%
 
 %%% The final codes of the paper will be posted once the paper accepted %%%
+%%%% Those codes maybe submitted with the orginal paper to the Jouranl %%%%
