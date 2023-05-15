cea05df1615179ec97711a2e59afc551d01c4a07
Saleh Alawsh <44799605+alawsh21@users.noreply.github.com>
2023-05-15 14:19:06 +0300

    Delete Reedme.m

--- Reedme.m
+++
@@ -1,5 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%% This code was writen by Dr. Saleh Alawsh %%%%%%%%%%%%%%%%
-
-%%% The final codes of the paper will be posted once the paper accepted %%%
-%%% Those codes maybe submitted with the original paper to the Journal %%%
